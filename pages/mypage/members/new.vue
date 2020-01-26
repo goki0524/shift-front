7aa44d89b6b084ddcc1902bd115abbde0c636412
goki sakaguchi <goki0524@gmail.com>
2020-01-26 23:18:18 +0900

    Finish Add member page

--- pages/mypage/members/new.vue
+++ pages/mypage/members/new.vue
@@ -10,6 +10,12 @@
       >
         <v-row justify="center">
           <v-col cols="12" sm="10" md="8" lg="6">
+            <v-alert v-if="postSuccess" type="success">
+              メンバーを追加しました。
+            </v-alert>
+            <v-alert v-if="postSuccess == false" type="error">
+              メンバーの追加に失敗しました。
+            </v-alert>
             <v-card ref="form">
               <v-toolbar flat>
                 <v-toolbar-title class="title">データを入力してメンバーを登録してください</v-toolbar-title>
@@ -18,7 +24,6 @@
 
                 <header class="input-lable">名前</header>
                 <v-text-field
-                  ref="firstName"
                   v-model="firstName"
                   :rules="[rules.required]"
                   placeholder="姓"
@@ -26,19 +31,22 @@
                 ></v-text-field>
 
                 <v-text-field
-                  ref="lastName"
                   v-model="lastName"
                   :rules="[rules.required]"
                   placeholder="名"
                   required
                 ></v-text-field>
 
+                <header class="input-lable">ID</header>
+                <v-text-field
+                  v-model="customId"
+                  type="number"
+                ></v-text-field>
+
                 <header class="input-lable">メールアドレス</header>
                 <v-text-field
-                  ref="email"
                   v-model="email"
                   :rules="[rules.email]"
-                  
                   placeholder="email@example.com"
                   required
                 ></v-text-field>
@@ -91,8 +99,34 @@
                   v-model="employmentTypeName"
                   :items="getEmploymentTypeNames"
                 ></v-select>
-                
+
+                <header class="input-lable">新卒/中途</header>
+                {{getRecruitmentType}} | {{recruitmentTypeName}}
+                <v-select
+                  v-model="recruitmentTypeName"
+                  :items="getRecruitmentTypeNames"
+                ></v-select>
+
+                <header class="input-lable">メモ</header>
+                {{memo}}
+                <v-textarea
+                  v-model="memo"
+                  counter
+                ></v-textarea>
               </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  text
+                  color="#00c58e"
+                  class="post-btn"
+                  :loading="isLoading"
+                  @click="storeMember"
+                >
+                  追加する
+                </v-btn>
+              </v-card-actions>
+
             </v-card>
           </v-col>
         </v-row>
@@ -109,22 +143,55 @@
 .input-lable {
   font-weight: bold;
 }
+.post-btn {
+  display: block;
+  margin: 0 0 0 auto;
+}
 </style>
 
 <script>
   // import { mapGetters } from 'vuex'
+  const API_URL = 'http://127.0.0.1:3333/api/v1/company/members'
+  import querystring from 'querystring'
   import NavDrawer from '~/components/NavDrawer.vue'
   import Footer from '~/components/Footer.vue'
-  // const API_URL = 'http://127.0.0.1:3333/api/v1/company'
   import jobCategoryJson from '~/utils/member/jobCategoryId.json'
   import positionIdJson from '~/utils/member/positionId.json'
   import employmentTypeJson from '~/utils/member/employmentType.json'
+  import recruitmentTypeJson from '~/utils/member/recruitmentType.json'
 
   export default {
     middleware: 'authenticated',
     components: {
       NavDrawer,
       Footer,
+    },
+    data() {
+      return {
+        drawer: false,
+        isLoading: false,
+        postSuccess:null,
+        error: null,
+
+        firstName: '',
+        lastName: '',
+        customId: null,
+        email: '',
+        gender: null,
+        jobCategoryName: '',
+        positionName: '',
+        employmentTypeName: '',
+        recruitmentTypeName: '',
+        year: null,
+        month: null,
+        date: null,
+        birthday: null,
+        memo: '',
+        rules: {
+          email: v => (v || '').match(/@/) ? true : 'Emailアドレスが正しくありません',
+          required: v => !!v || 'この項目は必須です',
+        }
+      }
     },
     computed: {
       getYearItems() {
@@ -168,29 +235,20 @@
       getEmploymentType() {
         const employmentType = this.getMemberMappingData(employmentTypeJson, 'employmentTypeJson', 'name', this.employmentTypeName)
         return employmentType
-      }
-    },
-    data() {
-      return {
-        drawer: false,
-        firstName: '',
-        lastName: '',
-        customId: null,
-        email: '',
-        gender: null,
-        jobCategoryName: '',
-        positionName: '',
-        employmentTypeName: '',
-        recruitmentType: null,
-        year: null,
-        month: null,
-        date: null,
-        birthday: null,
-        memo: '',
-        rules: {
-          email: v => (v || '').match(/@/) ? true : 'Emailアドレスが正しくありません',
-          required: v => !!v || 'この項目は必須です',
-        }
+      },
+      getRecruitmentTypeNames() {
+        const recruitmentTypeNames = recruitmentTypeJson.recruitmentType.map(i => {
+          return i['name']
+        })
+        return recruitmentTypeNames
+      },
+      getRecruitmentType() {
+        const recruitmentType = this.getMemberMappingData(recruitmentTypeJson, 'recruitmentTypeJson', 'name', this.recruitmentTypeName)
+        return recruitmentType
+      },
+      getBirthday() {
+        const birthday = `${this.year}-${this.month}-${this.date}`
+        return birthday
       }
     },
     methods: {
@@ -222,6 +280,15 @@
               let targetObj3 = targetArr3.shift()
               if (typeof targetObj3 === 'object') {
                 return targetObj3.hasOwnProperty('name') ? targetObj3.id : null
+              }
+              break
+            case 'recruitmentTypeJson':
+              let targetArr4 = model.recruitmentType.filter((obj) => {
+                return obj.id === val
+              })
+              let targetObj4 = targetArr4.shift()
+              if (typeof targetObj4 === 'object') {
+                return targetObj4.hasOwnProperty('name') ? targetObj4.id : null
               }
               break
             default:
@@ -256,12 +323,66 @@
                 return targetObj3.hasOwnProperty('id') ? targetObj3.id : null
               }
               break
+            case 'recruitmentTypeJson':
+              let targetArr4 = model.recruitmentType.filter((obj) => {
+                return obj.name === val
+              })
+              let targetObj4 = targetArr4.shift()
+              if (typeof targetObj4 === 'object') {
+                return targetObj4.hasOwnProperty('id') ? targetObj4.id : null
+              }
+              break
             default:
               return null
           }
         }
+      },
+
+      async storeMember() {
+
+        const data = {
+          customId: this.customId,
+          jobCategoryId: this.getJobCategoryId,
+          positionId: this.getPositionId,
+          employmentType: this.getEmploymentType,
+          recruitmentType: this.getRecruitmentType,
+          firstName: this.firstName,
+          lastName: this.lastName,
+          email: this.email,
+          gender: this.gender,
+          birthday: this.getBirthday,
+          memo: this.memo,
+        }
+
+        this.isLoading = true
+        const accessToken = this.$store.getters['auth/accessToken']
+        const token = accessToken.token
+        const response = await this.$axios
+          .$post(API_URL, querystring.stringify({ ...data }),
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            }
+          })
+          .catch(error => {
+            console.log('response error trial', error)
+          })
+        this.isLoading = false
+        console.log(response)
+
+        if (response.length > 0){
+          if (response[0].hasOwnProperty('message')) {
+            this.error = response[0].message
+            this.postSuccess = false
+            console.log(this.error)
+          }
+        } else {
+          this.postSuccess = true
+        }
       }
-    }
+
+    },
+
     // async asyncData({ $axios, query, store }) {
     //   const accessToken = store.getters['auth/accessToken']
     //   const token = accessToken.token
